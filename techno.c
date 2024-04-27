066da5620a111b93c214852aa0fae57a362f94c9
Tero Parviainen <tero@teropa.info>
2024-04-27 15:58:49 +0300

    beat & envelope

--- techno.c
+++ techno.c
@@ -1,13 +1,16 @@
 #include <math.h>
 
 static float outputBuffer[128];
-unsigned long long samples = 0;
+unsigned long long tSamples = 0;
 
 float* makeSomeTechno() {
   for (unsigned char i = 0; i < 128; i++) {
-    float seconds = samples++ / 48000.0;
+    float tSeconds = tSamples++ / 48000.0;
+    float tBeats = tSeconds * 2.0;
+    float tInBeat = fmodf(tBeats, 1.0);
 
-    float val = sinf(seconds * 2.0 * M_PI * 220.0) * 0.2;
+    float env = 0.2 * (1.0 - tInBeat);
+    float val = sinf(tSeconds * 2.0 * M_PI * 220.0) * env;
 
     outputBuffer[i] = val;
   }
