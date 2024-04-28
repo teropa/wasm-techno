10bcda2e53dc60933d19ed2bc67877d6d4abf4db
Tero Parviainen <tero@teropa.info>
2024-04-28 12:32:42 +0300

    adjustments

--- techno.c
+++ techno.c
@@ -7,12 +7,12 @@
 static float outputBuffer[128];
 
 unsigned long long tSamples = 0;
-double kickPhase = 0.0;
+double kickPhase = 0.0, bassPhase = 0.0;
 
 float* makeSomeTechno() {
   for (unsigned char i = 0; i < 128; i++) {
     double tSeconds = tSamples++ * SAMPLE_DURATION;
-    double tBeats = tSeconds * 2.0;
+    double tBeats = tSeconds * 2.0; // 120 BPM
     double tInBeat = fmod(tBeats, 1.0);
     double tSixteenths = tBeats * 4.0;
     double tInSixteenth = fmod(tSixteenths, 1.0);
@@ -23,8 +23,11 @@ float* makeSomeTechno() {
     double kickEnv = powf(1.0 - tInBeat, 3.0) * 0.15;
     double kick = sinf(kickPhase) * kickEnv;
 
-    double bassEnv = ((int)tSixteenths) % 4 == 0 ? 0.0 : pow(2.0, -(4.0 * tInSixteenth + 0.01 / tInSixteenth)) * 0.2;
-    double bass = tanh(sinf(tSeconds * TWO_PI * 75.0) * 2.5) * bassEnv;
+    double bassPitch = 50.0;
+    bassPhase = fmod(bassPhase + SAMPLE_DURATION * TWO_PI * bassPitch, TWO_PI);
+    int downbeat = ((int)tSixteenths) % 4 == 0;
+    double bassEnv = downbeat ? 0.0 : pow(2.0, -(4.0 * tInSixteenth + 0.01 / tInSixteenth)) * 0.4;
+    double bass = tanh(sinf(bassPhase) * 2.5) * bassEnv;
 
     outputBuffer[i] = kick + bass;
   }
